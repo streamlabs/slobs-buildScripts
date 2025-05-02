8b3b2008bb3e399692ba666c5a2fa89595621595
Richard Osborne <rosborne255@yahoo.com>
2025-05-02 18:56:19 -0500

    remove SDK location
    * The cmake files were recently updated to dynamically find the xcode sdk using xcrun

--- rebuild-slobs.sh
+++ rebuild-slobs.sh
@@ -1,5 +1,5 @@
 cd ../obs-studio/
 
 rm -rf build_macos
-cmake -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk --preset macos -DCMAKE_INSTALL_PREFIX=build_macos/packed_build
+cmake --preset macos -DCMAKE_INSTALL_PREFIX=build_macos/packed_build
 
