855a83d0e691e06c10169673468b97c3e3658723
Richard Osborne <rosborne255@yahoo.com>
2025-05-29 09:07:22 -0500

    use correct lowercase filename for copy-osn script

--- update-everything-from-slobs.sh
+++ update-everything-from-slobs.sh
@@ -5,7 +5,7 @@ exit_status=$?
 
 if [ $exit_status -eq 0 ]; then
   echo "Script slobs-to-desktop executed successfully."
-  "./copy-OSN-to-desktop.sh"
+  "./copy-osn-to-desktop.sh"
 else
   echo "building slobs failed with exit code $exit_status."
 fi
