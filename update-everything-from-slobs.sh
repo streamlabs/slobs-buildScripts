0672872cf08addaea2c44a70b45ecd1e2cdc7f0f
Richard Osborne <rosborne255@yahoo.com>
2025-05-29 09:58:19 -0500

    add step to ensure LibOBS is updated on streamlabs desktop

--- update-everything-from-slobs.sh
+++ update-everything-from-slobs.sh
@@ -6,6 +6,9 @@ exit_status=$?
 if [ $exit_status -eq 0 ]; then
   echo "Script slobs-to-desktop executed successfully."
   "./copy-osn-to-desktop.sh"
+
+  # OSN downloads a slobs artifact stomping LibOBS. So we will overwrite the libOBS with our latest local code.
+  "./slobs-to-desktop.sh"
 else
   echo "building slobs failed with exit code $exit_status."
 fi
