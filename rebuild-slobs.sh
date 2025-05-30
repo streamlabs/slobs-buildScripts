ee8f4f8b77aebc0022fdd3d14deb9229326220ff
RICKPORTABLE\rosbo <rosborne255@yahoo.com>
2025-05-30 09:53:44 -0500

    update to support windows platform

--- rebuild-slobs.sh
+++ rebuild-slobs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd ../obs-studio/
 
-rm -rf build_macos
-cmake --preset macos -DCMAKE_INSTALL_PREFIX=build_macos/packed_build -DOBS_PROVISIONING_PROFILE="$OBS_PROVISIONING_PROFILE" -DOBS_CODESIGN_TEAM="$OBS_CODESIGN_TEAM" -DOBS_CODESIGN_IDENTITY="$OBS_CODESIGN_IDENTITY"
+platform="macos"
+buildFolder="build_macos"
+if [ "$1" == "windows-x64" ] || [ "$1" == "windows-ci-x64" ]; then
+  platform=$1
+  buildFolder="build_x64"
+else
+  echo "platform: $platform"
+fi
+
+rm -rf $buildFolder
+cmake --preset $platform -DCMAKE_INSTALL_PREFIX=$buildFolder/packed_build -DOBS_PROVISIONING_PROFILE="$OBS_PROVISIONING_PROFILE" -DOBS_CODESIGN_TEAM="$OBS_CODESIGN_TEAM" -DOBS_CODESIGN_IDENTITY="$OBS_CODESIGN_IDENTITY"
 
