d779c12ac45406f1e6a5ecf599c3dc998b110ee8
Richard Osborne <rosborne255@yahoo.com>
2025-05-30 10:07:24 -0500

    add placeholder(?) fix for realm
    sometimes realm does not build on dev machine depending upon what they have installed

--- rebuild-desktop.sh
+++ rebuild-desktop.sh
@@ -6,6 +6,7 @@ exit_status=$?
 
 if [ $exit_status -eq 0 ]; then
   echo "built desktop successfully."
+  yarn add realm # Hack to work around 'realm' build error
   yarn compile
 else
   echo "failed building desktop with exit code $exit_status."
