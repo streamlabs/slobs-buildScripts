94a0a7aadbfd0abb60369f71553545aa40e0e837
RICKPORTABLE\rosbo <rosborne255@yahoo.com>
2025-05-30 12:14:34 -0500

    improve slobs rebuild script
    * Check for sibling folders (windows build will install into OSN folder)

--- rebuild-slobs.sh
+++ rebuild-slobs.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-cd ../obs-studio/
+if [ ! -d "../obs-studio" ]; then
+  echo "Error: 'obs-studio' directory is not found."
+  exit 1
+fi
 
+if [ ! -d "../obs-studio-node" ]; then
+  echo "Error: 'obs-studio-node' directory is not found."
+  exit 1
+fi
+
+cd ..
+origin_dir=$(pwd) # Save the starting directory
+cd obs-studio/
+
+os="darwin"
 platform="macos"
-buildFolder="build_macos"
+buildFolder="build_macos/packed_build"
 if [ "$1" == "windows-x64" ] || [ "$1" == "windows-ci-x64" ]; then
   platform=$1
-  buildFolder="build_x64"
+  buildFolder="$origin_dir/obs-studio-node/build/libobs-src"
+  os="windows"
 else
-  os="darwin"
   echo "platform: $platform"
 fi
 
-rm -rf $buildFolder
-cmake --preset $platform -DCMAKE_INSTALL_PREFIX=$buildFolder/packed_build -DOBS_PROVISIONING_PROFILE="$OBS_PROVISIONING_PROFILE" -DOBS_CODESIGN_TEAM="$OBS_CODESIGN_TEAM" -DOBS_CODESIGN_IDENTITY="$OBS_CODESIGN_IDENTITY"
+rm -rf "$buildFolder"
+cmake --preset $platform -DCMAKE_INSTALL_PREFIX=$buildFolder -DOBS_PROVISIONING_PROFILE="$OBS_PROVISIONING_PROFILE" -DOBS_CODESIGN_TEAM="$OBS_CODESIGN_TEAM" -DOBS_CODESIGN_IDENTITY="$OBS_CODESIGN_IDENTITY"
 
-if ["$os" == "darwin"]; then
-  # Relaunch Xcode; this way all the targets wil be refreshed properly
+if [ "$os" == "darwin" ]; then
+  # Relaunch Xcode; this way all the targets will be refreshed properly
   echo "Attempting to re-open the xcode project"
-  open $buildFolder/obs-studio.xcodeproj
+  open "$buildFolder/obs-studio.xcodeproj"
 fi
