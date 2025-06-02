a2f494dec9feb44614f84cd24759efea10d48796
Richard Osborne <rosborne255@yahoo.com>
2025-06-02 09:19:15 -0500

    remove dead comments & add error checking

--- run-local-arm64-desktop-app.sh
+++ run-local-arm64-desktop-app.sh
@@ -3,11 +3,11 @@
 
 # Reload bash profile etc incase dev changed env variable
 source ~/.zshrc
-cd ../desktop/dist/mac-arm64
-#cd ./Streamlabs\ Desktop.app/Contents/MacOS
-open ~/Library/Application\ Support/slobs-client/node-obs/logs/
 
-# open ./Streamlabs\ Desktop.app --args skip-update
+# Assumes artifact was built on arm64
+cd ../desktop/dist/mac-arm64  || { echo "Error: Failed to change to directory mac-arm64 make sure the artifact has been built by the yarn package:mac-arm64 command"; exit 1; }
+
+open ~/Library/Application\ Support/slobs-client/node-obs/logs/
 
 # Run the app directly so we can capture stdout and skip auto-updater
 ./Streamlabs\ Desktop.app/Contents/MacOS/Streamlabs\ Desktop --skip-update
