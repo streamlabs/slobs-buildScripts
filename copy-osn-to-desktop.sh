d354cb805fda7ee31e66fa99bb33b0aa0bb19b11
Richard Osborne <rosborne255@yahoo.com>
2025-06-02 12:00:36 -0500

    update copy-osn script to ONLY copy OSN bins
    * do not copy over slobs binaries. users need to run 'copy-slobs-artifacts.sh" for that. Everything is more intuitive this way
    and the scripts only do one thing.

--- copy-osn-to-desktop.sh
+++ copy-osn-to-desktop.sh
@@ -17,21 +17,23 @@ else
   exit 1
 fi
 
+# Remove previous artifacts. Force timestamps to get updated.
+rm -rf $origin_dir/obs-studio-node/streamlabs-build.app/distribute/obs-studio-node
+rm -rf $origin_dir/obs-studio-node/streamlabs-build.app/obs-studio-server/RelWithDebInfo
+
+# Build RelWithDebInfo artifact and install into distribute folder.
 cmake --build streamlabs-build.app --target install --config RelWithDebInfo
 
 exit_status=$?
 
 if [ $exit_status -eq 0 ]; then
   echo "copying the files."
-    cd $origin_dir/obs-studio-node/streamlabs-build.app/distribute/obs-studio-node
+  cd $origin_dir/obs-studio-node/streamlabs-build.app/distribute/obs-studio-node
 
-    rm -rf $origin_dir/desktop/node_modules/obs-studio-node/bin
-    cp -R ./bin $origin_dir/desktop/node_modules/obs-studio-node
-    rm -rf $origin_dir/desktop/node_modules/obs-studio-node/Frameworks
-    rm -rf $origin_dir/desktop/node_modules/obs-studio-node/PlugIns
-    cp -R ./Frameworks $origin_dir/desktop/node_modules/obs-studio-node
-    cp -R ./PlugIns $origin_dir/desktop/node_modules/obs-studio-node
-    cp obs_studio_client.0.3.21.0.node $origin_dir/desktop/node_modules/obs-studio-node
+  rm -rf $origin_dir/desktop/node_modules/obs-studio-node/bin
+  # Copy obs-studio-server binary
+  cp -Rv ./bin $origin_dir/desktop/node_modules/obs-studio-node
+  cp obs_studio_client.0.3.21.0.node $origin_dir/desktop/node_modules/obs-studio-node
 else
   echo "building osn failed with exit code $exit_status."
 fi
--- update-everything-from-slobs.sh
+++ update-everything-from-slobs.sh
@@ -8,11 +8,6 @@ if [ $exit_status -eq 0 ]; then
   echo "Script slobs-to-desktop executed successfully."
   cd $origin_dir
   "./copy-osn-to-desktop.sh"
-
-  cd $origin_dir
-  pwd
-  # OSN downloads a slobs artifact stomping LibOBS. So we will overwrite the libOBS with our latest local code.
-  "./copy-slobs-artifacts.sh"
 else
   echo "building slobs failed with exit code $exit_status."
 fi
