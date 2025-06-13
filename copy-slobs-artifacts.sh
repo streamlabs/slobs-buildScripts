b5e2ce48e1a4ec4d968e86804fef6c8e19087470
Richard Osborne <rosborne255@yahoo.com>
2025-06-13 09:58:16 -0500

    remove old comments from copy-slobs-artifacts.sh / fix exit code

--- copy-slobs-artifacts.sh
+++ copy-slobs-artifacts.sh
@@ -10,13 +10,7 @@ if [ "$ostype" == "Darwin" ]; then
   echo "Script $0 is running on macOS."
 elif [[ "$ostype" == MINGW* || "$ostype" == CYGWIN* ]]; then
   echo "$0 has nothing to do on Windows as long as build scripts for SLOBS/OSN are working."
-  # TODO: Windows build scripts for SLOBS/OSN should already be copying over the bins.
-  #cp  -rv build_x64/rundir/RelWithDebInfo/bin/64bit/* ../desktop/node_modules/obs-studio-node/
-  #cp  -rv build_x64/rundir/RelWithDebInfo/obs-plugins/64bit/* ../desktop/node_modules/obs-studio-node/
-
-  # copy the artifacts developer compiled from vstudio
-  #cp  -rv build_x64/plugins/obs-browser/RelWithDebInfo/* ../desktop/node_modules/obs-studio-node/
-  exit 1
+  exit 0
 else
   echo "Unsupported operating system: $ostype"
   exit 1
@@ -41,16 +35,11 @@ cp  -Rv "$build_folder/OBS.app/Contents/Frameworks" ../desktop/node_modules/obs-
 cp  -Rv "$build_folder/OBS.app/Contents/Plugins" ../desktop/node_modules/obs-studio-node
 
 # copy to OSN
-# Disabled removing the previous artifact. This will require Obs-studio-node xcodeproj to be rebuilt because it cant find obs.h
-#rm -rf ../obs-studio-node/streamlabs-build.app/libobs-src/Frameworks
 
 # make sure old artifacts are removed.
 rm -rf ./obs-studio-node/streamlabs-build.app/libobs-src/OBS.app
 cp -R "$build_folder/OBS.app/Contents/Frameworks" ../obs-studio-node/streamlabs-build.app/libobs-src/
 cp -R "$build_folder/OBS.app" ../obs-studio-node/streamlabs-build.app/libobs-src
 
-# Disabled removing the previous artifact. This will require Obs-studio-node xcodeproj to be rebuilt because it cant find obs.h
-#rm -rf ../obs-studio-node/streamlabs-build.app/libobs-src/include
-#rm -rf ../obs-studio-node/streamlabs-build.app/libobs-src/lib
 cp  -R ./build_macos/packed_build/include ../obs-studio-node/streamlabs-build.app/libobs-src/
 cp  -R ./build_macos/packed_build/lib ../obs-studio-node/streamlabs-build.app/libobs-src/
