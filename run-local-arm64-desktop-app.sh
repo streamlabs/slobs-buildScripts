64d2bc21b4f975399301397515228faca4f1237e
Richard Osborne <rosborne255@yahoo.com>
2025-06-17 08:48:00 -0500

    update script to work on x86_64 machines

--- run-local-arm64-desktop-app.sh
+++ run-local-arm64-desktop-app.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# Run Streamlabs Desktop w/o triggering the updater and open the logs folder (although stdout and stderr will appear in Terminal)
+# Run Streamlabs Desktop w/o triggering the updater and open the logs folder (although stdout and stderr will appear in Terminal).
 HOME_DIR=$(dirname "$(realpath "$0")")
 cd "$HOME_DIR" # make sure we start in the home folder
 # Reload bash profile etc incase dev changed env variable
 source ~/.zshrc
 
-# Assumes artifact was built on arm64
-cd ../desktop/dist/mac-arm64  || { echo "Error: Failed to change to directory mac-arm64 make sure the artifact has been built by the yarn package:mac-arm64 command"; exit 1; }
+ARCH=$(uname -m)
+if [[ "$ARCH" == "arm64" ]]; then
+    cd ../desktop/dist/mac-arm64  || { echo "Error: Failed to change to directory mac-arm64 make sure the artifact has been built by the yarn package:mac-arm64 command"; exit 1; }
+elif [[ "$ARCH" == "x86_64" ]]; then
+    cd ../desktop/dist/mac  || { echo "Error: Failed to change to directory mac make sure the artifact has been built by the yarn package:mac command"; exit 1; }
+else
+    echo "Unknown architecture: $ARCH"
+fi
 
 open ~/Library/Application\ Support/slobs-client/node-obs/logs/
 
